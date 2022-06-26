20a97b0072b85ea6dcc84673a7dfafbaad9d7b48
RollMan <qwpmb554@gmail.com>
2022-06-26 17:30:42 +0900

    batch shape is also required

--- bindsnet/network/topology.py
+++ bindsnet/network/topology.py
@@ -860,7 +860,9 @@ class MaxPool2dConnection(AbstractConnection):
         self.padding = _pair(padding)
         self.dilation = _pair(dilation)
 
-        self.register_buffer("firing_rates", torch.zeros(source.shape))
+        self.register_buffer(
+            "firing_rates", torch.zeros([source.batch_size, *source.shape])
+        )
 
     def compute(self, s: torch.Tensor) -> torch.Tensor:
         # language=rst
